fa6b522cb54d556e938db22edbdf377d463b160c
KingFlippyNips <varunmaurya37@gmail.com>
2020-05-02 06:09:06 +0530

    Added CommandContext to CommandAPI Solution

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -2,36 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace CommandAPI
-{
-    public class Startup
-    {
+namespace CommandAPI {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public IConfiguration Configuration { get; }
+        public Startup (IConfiguration configuration) => Configuration = configuration;
+        public void ConfigureServices (IServiceCollection services) {
             services.AddControllers();
+            services.AddDbContext<CommandContext>(option => option.UseNpgsql 
+                (Configuration.GetConnectionString ("PostgreSqlConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
